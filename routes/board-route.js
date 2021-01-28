f23e30321c4fc116f1b5e5550f5ba0f31054a1f9
doej96 <doej96@naver.com>
2021-01-28 11:28:34 +0900

    ...

--- routes/board-route.js
+++ routes/board-route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { upload } = require('../modules/multers')
 const { pool } = require('../modules/mysql-pool');
 const { err, alert } = require('../modules/util');
+const moment = require('moment')
+const path = require('path')
 const pugs = { 
 	css: 'board', 
 	js: 'board', 
@@ -13,6 +15,28 @@ const pugs = {
 router.get('/', (req, res, next) => {
 	res.render('board/list', { ...pugs });
 });
+
+router.get('/', async(req, res, next) => {
+  try {
+    let sql = 'SELECT * FROM board ORDER BY id DESC'
+    const r = await pool.query(sql);
+    const rs = r[0].map((v)=> {
+      v.wdate = moment(v.wdate).format('YYYY-MM-DD');
+      if(v.savefile) {
+        let ext = path.extname(v.savefile).substr(1).toLowerCase();
+        ext = (ext == 'jpeg') ? 'jpg' : ext;
+        ext = ext.substr(0, 3);
+        v.icon = `/img/ext/${ext}.png`;
+      }
+      else v.icon = '/img/empty.png'
+      return v;
+    });
+    res.render('board/list', {...pugs, rs})
+  }
+  catch(e) {
+    next(err(e.message))
+  }
+})
 
 router.get('/create', (req, res, next) => {
 	const pug = { ...pugs, tinyKey: process.env.TINY_KEY }
